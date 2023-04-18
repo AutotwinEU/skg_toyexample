f63d2e893218005ebd33a52b7f4dba2f5c2d9807
Ava-S <ava.swevels@gmail.com>
2023-04-18 13:31:13 +0200

    Adapt methods to use Locations instead of Class_Station

--- ekg_creator_custom/cypher_queries/custom_query_library.py
+++ ekg_creator_custom/cypher_queries/custom_query_library.py
@@ -18,8 +18,10 @@ class CustomCypherQueryLibrary:
             MATCH (c_start:Class_sensor)
             WHERE NOT EXISTS ((:Class) - [:$df_c_type] -> (c_start))
             WITH c_start, "SourceStation"+c_start.cID as id
-            MERGE (c_start) - [:COMPOSED] -> (:Class:Class_station {classType: "station", type: "Source", sensor: c_start.cID,
-                                                           ID: id, uID:"Station_"+id})
+            MERGE (s:Entity:Station {entityType: "Station", type: "Source", sensors: [c_start.cID],
+                                             ID: id, uID:"Station_"+id})
+            MERGE (c_start) - [:OCCURS_AT] -> (l:Location {locationType: "station", ID: id})
+            MERGE (s) - [:LOCATED_AT] -> (l)
         '''
 
         query_str = Template(query_str).substitute(df_c_type=f"DF_C_{entity_type.upper()}")
@@ -31,8 +33,10 @@ class CustomCypherQueryLibrary:
                 MATCH (c_end:Class_sensor)
                 WHERE NOT EXISTS ((c_end) - [:$df_c_type] -> (:Class))
                 WITH c_end, "SinkStation"+c_end.cID as id
-                MERGE (c_end) - [:COMPOSED] -> (:Class:Class_station {classType: "station", type: "Sink", sensor: c_end.cID,
-                                                           ID: id, uID:"Station_"+id})
+                MERGE (s:Entity:Station {entityType: "Station", type: "Sink", sensors: [c_end.cID],
+                                             ID: id, uID:"Station_"+id})
+                MERGE (c_end) - [:OCCURS_AT] -> (l:Location {locationType: "station", ID: id})
+                MERGE (s) - [:LOCATED_AT] -> (l)
             '''
 
         query_str = Template(query_str).substitute(df_c_type=f"DF_C_{entity_type.upper()}")
@@ -47,9 +51,12 @@ class CustomCypherQueryLibrary:
                     UNWIND range(1,size(classList)-3,2) AS i
                     WITH classList[i] as first, classList[i+1] as second
                     WITH first, second, "ProcessingStation"+first.cID+second.cID as id
-                    MERGE (first) - [:COMPOSED] -> (:Class:Class_station {classType: "station", type: "Processing", 
-                                                start_sensor: first.cID, end_sensor: second.cID,
-                                                ID: id, uID:"Station_"+id}) <- [:COMPOSED] - (second)
+                    MERGE (s:Entity:Station {entityType: "Station", type: "Processing", 
+                                             sensors: [first.cID, second.cID],
+                                              ID: id, uID:"Station_"+id})
+                    MERGE (first) - [:OCCURS_AT] -> (l:Location {locationType: "station", 
+                                                ID: id}) <- [:OCCURS_AT] - (second)
+                    MERGE (s) - [:LOCATED_AT] -> (l)
                 '''
 
         query_str = Template(query_str).substitute(df_c_type=f"DF_C_{entity_type.upper()}")
@@ -58,19 +65,15 @@ class CustomCypherQueryLibrary:
     @staticmethod
     def get_observe_events_to_station_aggregation_query():
         query_str = '''
-            MATCH (e:Event) - [:OBSERVED] -> (c:Class_sensor) - [:COMPOSED] -> (s:Class_station)
-            MERGE (e) - [:OBSERVED] -> (s)
+            MATCH (e:Event) - [:OBSERVED] -> (c:Class_sensor) - [:OCCURS_AT] -> (l:Location)
+            MERGE (e) - [:OCCURRED_AT] -> (l)
         '''
         return Query(query_string=query_str, kwargs={})
 
     @staticmethod
-    def get_create_processing_station_entities_and_correlate_to_events_query():
+    def get_create_station_entities_and_correlate_to_events_query():
         query_str = '''
-            MATCH (e) - [:OBSERVED] ->  (cs:Class_station {type:"Processing"})
-            WITH e, cs, cs.type + cs.start_sensor+cs.end_sensor AS id
-            MERGE (s:Entity:Station {entityType: "Station", type: cs.type, 
-                                    start_sensor: cs.start_sensor, end_sensor: cs.end_sensor,
-                                    ID: id, uID:"Station_"+id})
+            MATCH (e) - [:OCCURRED_AT] ->  (l:Location) <- [:LOCATED_AT] - (s:Station)
             MERGE (e) - [:CORR] -> (s)
         '''
         return Query(query_string=query_str, kwargs={})
@@ -78,12 +81,13 @@ class CustomCypherQueryLibrary:
     @staticmethod
     def get_create_non_processing_station_entities_and_correlate_to_events_query():
         query_str = '''
-                MATCH (e) - [:OBSERVED] -> (cs:Class_station)
+                MATCH (e) - [:AT] -> (cs:Location)
                 WHERE cs.type <> "Processing"
-                WITH e, cs, cs.type + cs.sensor AS id
+                WITH e, cs, cs.ID AS id
                 MERGE (s:Entity:Station {entityType: "Station", type: cs.type, 
-                                        sensor: cs.sensor, 
+                                        sensor: cs.sensors[0], 
                                         ID: id, uID:"Station_"+id})
+                MERGE (s) - [:LOCATED_AT] -> (cs)
                 MERGE (e) - [:CORR] -> (s)
             '''
         return Query(query_string=query_str, kwargs={})
