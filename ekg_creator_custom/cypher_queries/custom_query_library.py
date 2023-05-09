fbcb3ffe82295dd6c0cce77107fbcb6093097bfb
Ava-S <ava.swevels@gmail.com>
2023-05-09 17:00:51 +0200

    - Add queries to write attributes to stations and request the log - Adapt queries to reflect schema change from Class_Station to Entity:Resource:Station

--- ekg_creator_custom/cypher_queries/custom_query_library.py
+++ ekg_creator_custom/cypher_queries/custom_query_library.py
@@ -15,10 +15,9 @@ class CustomCypherQueryLibrary:
             MATCH (c_start:Class {classType: "sensor"})
             WHERE NOT EXISTS ((:Class) - [:$df_c_type] -> (c_start))
             WITH c_start, "SourceStation"+c_start.cID as id
-            MERGE (s:Entity:Station {entityType: "Station", type: "Source", sensors: [c_start.cID],
+            MERGE (s:Entity:Resource:Station {entityType: "Station", type: "Source", sensors: [c_start.cID],
                                              ID: id, uID:"Station_"+id})
-            MERGE (c_start) - [:OCCURS_AT] -> (l:Location {locationType: "station", ID: id})
-            MERGE (s) - [:LOCATED_AT] -> (l)
+            MERGE (c_start) - [:OCCURS_AT] -> (s)
         '''
 
         return Query(query_str=query_str,
@@ -31,10 +30,9 @@ class CustomCypherQueryLibrary:
                 MATCH (c_end:Class {classType: "sensor"})
                 WHERE NOT EXISTS ((c_end) - [:$df_c_type] -> (:Class))
                 WITH c_end, "SinkStation"+c_end.cID as id
-                MERGE (s:Entity:Station {entityType: "Station", type: "Sink", sensors: [c_end.cID],
+                MERGE (s:Entity:Resource:Station {entityType: "Station", type: "Sink", sensors: [c_end.cID],
                                              ID: id, uID:"Station_"+id})
-                MERGE (c_end) - [:OCCURS_AT] -> (l:Location {locationType: "station", ID: id})
-                MERGE (s) - [:LOCATED_AT] -> (l)
+                MERGE (c_end) - [:OCCURS_AT] -> (s)
             '''
 
         return Query(query_str=query_str,
@@ -44,19 +42,19 @@ class CustomCypherQueryLibrary:
     def get_create_processing_stations_aggregation_query(entity_type):
         # language=sql
         query_str = '''
-                    MATCH p=(c_start:Class {classType: "sensor"}) - [:$df_c_type*] -> (c_end:Class {classType: "sensor"})
-                    WHERE NOT EXISTS ((c_end) - [:$df_c_type] -> (:Class {classType: "sensor"})) AND NOT EXISTS ((:Class {classType: "sensor"}) - [
+                    MATCH p=(c_start:Class {classType: "sensor"}) - [:$df_c_type*] -> (c_end:Class {classType: 
+                    "sensor"})
+                    WHERE NOT EXISTS ((c_end) - [:$df_c_type] -> (:Class {classType: "sensor"})) AND NOT EXISTS ((
+                    :Class {classType: "sensor"}) - [
                     :$df_c_type] -> (c_start))
                     WITH nodes(p) as classList
                     UNWIND range(1,size(classList)-3,2) AS i
                     WITH classList[i] as first, classList[i+1] as second
                     WITH first, second, "ProcessingStation"+first.cID+second.cID as id
-                    MERGE (s:Entity:Station {entityType: "Station", type: "Processing", 
+                    MERGE (s:Entity:Resource:Station {entityType: "Station", type: "Processing", 
                                              sensors: [first.cID, second.cID],
                                               ID: id, uID:"Station_"+id})
-                    MERGE (first) - [:OCCURS_AT] -> (l:Location {locationType: "station", 
-                                                ID: id}) <- [:OCCURS_AT] - (second)
-                    MERGE (s) - [:LOCATED_AT] -> (l)
+                    MERGE (first) - [:OCCURS_AT] -> (s) <- [:OCCURS_AT] - (second)
                 '''
 
         return Query(query_str=query_str,
@@ -66,11 +64,26 @@ class CustomCypherQueryLibrary:
     def get_observe_events_to_station_aggregation_query():
         # language=sql
         query_str = '''
-            MATCH (e:Event) - [:OBSERVED] -> (c:Class {classType: "sensor"}) - [:OCCURS_AT] -> (l:Location)
-            MERGE (e) - [:OCCURRED_AT] -> (l)
+            MATCH (e:Event) - [:OBSERVED] -> (c:Class {classType: "sensor"}) - [:OCCURS_AT] -> (s:Station)
+            MERGE (e) - [:CORR] -> (s)
         '''
 
         return Query(query_str=query_str)
+
+    @staticmethod
+    def get_connect_stations_queries(entity_type):
+        # language=sql
+        query_str = '''
+            MATCH (s1:Station) <- [:OCCURS_AT] - (c1:Class) - [:$df_c_type] -> (c2:Class) - [:OCCURS_AT] -> (s2:Station)
+            WHERE s1 <> s2
+            MERGE (s1) - [:CONN {movedEntity: "$entity_type"}] -> (s2)
+        '''
+
+        return Query(query_str=query_str,
+                     template_string_parameters={
+                         "df_c_type": f"DF_C_{entity_type.upper()}",
+                         "entity_type": entity_type
+                     })
 
     @staticmethod
     def get_create_station_entities_and_correlate_to_events_query():
@@ -97,3 +110,59 @@ class CustomCypherQueryLibrary:
             '''
 
         return Query(query_str=query_str)
+
+    @staticmethod
+    def get_read_log_query():
+        # language=sql
+        query_str = '''
+            MATCH (e:Event)-[:CORR]->(c:Entity:Station)
+            RETURN e.timestamp AS time, c.ID AS station, c.type AS station_type, e.pizzaId AS part,
+            CASE WHEN c.type = "Source" THEN "EXIT"
+                 WHEN c.type = "Sink" THEN "ENTER"
+                 WHEN e.sensor = c.sensors[0] THEN "ENTER" ELSE "EXIT"
+                 END AS activity
+            ORDER BY time, ID(e)
+        '''
+
+        return Query(query_str=query_str)
+
+    @staticmethod
+    def get_write_attributes_to_stations(station, buffer_capacity, machine_capacity, processing_time):
+        # language=sql
+        query_str = '''
+            MATCH (c:Entity:Station {ID: "$station"})
+                   SET c.buffer_capacity = $buffer_capacity,
+                       c.machine_capacity = $machine_capacity,
+                       c.processing_time_mean = $mean,
+                       c.processing_time_std = $std
+        '''
+
+        return Query(query_str=query_str,
+                     template_string_parameters={
+                         "station": station,
+                         "buffer_capacity": buffer_capacity,
+                         "machine_capacity": machine_capacity,
+                         "mean": processing_time['mean'],
+                         "std": processing_time['std']
+                     })
+
+    @staticmethod
+    def get_write_attributes_to_connections(connection, routing_probability, transfer_time):
+        # language=sql
+        query_str = '''
+                           MATCH (:Entity:Station {ID: "$connection_from"})
+                                 -[conn:CONN {movedEntity: "Pizza"}]->
+                                 (:Entity:Station {ID: "$connection_to"})
+                           SET conn.routing_probability = $routing_probability,
+                               conn.transfer_time_mean = $mean,
+                               conn.transfer_time_std = $std
+                    '''
+
+        return Query(query_str=query_str,
+                     template_string_parameters={
+                         "connection_from": connection[0],
+                         "connection_to": connection[1],
+                         "routing_probability": routing_probability,
+                         "mean": transfer_time['mean'],
+                         "std": transfer_time['std']
+                     })
