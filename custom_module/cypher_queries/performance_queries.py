a421cc3c964a383cc9b51dd4795484ad034f06be
Freek van den Berg <f.g.b.v.d.berg@tue.nl>
2023-12-19 14:20:51 +0100

    added performance high-level store in db

--- custom_module/cypher_queries/performance_queries.py
+++ custom_module/cypher_queries/performance_queries.py
@@ -3,12 +3,15 @@ from promg import Query
 
 class PerformanceQueryLibrary:
     @staticmethod
+    def delete_performance_artifacts():
+        query_str = '''MATCH(c:Performance) detach delete c'''
+        return Query(query_str=query_str)
+
+    @staticmethod
     def create_main_performance_nodes():
-        # language=sql
         query_str = '''
             CREATE(n1:MainPerformance:Performance),(n2:MainLatency:Performance),(n3:MainUtilization:Performance),
             (n4:MainQueue:Performance),(n5:MainFlow:Performance)
             CREATE(n1)-[:HAS_PERFORMANCE]->(n2),(n1)-[:HAS_PERFORMANCE]->(n3),(n1)-[:HAS_PERFORMANCE]->
             (n4),(n1)-[:HAS_PERFORMANCE]->(n5)'''
-
         return Query(query_str=query_str)
--- custom_module/modules/pizza_performance.py
+++ custom_module/modules/pizza_performance.py
@@ -8,4 +8,23 @@ class PizzaPerformanceModule:
 
     def add_performance_to_skg(self):
         print("add_performance_to_skg")
+        self.connection.exec_query(pfql.delete_performance_artifacts)
         self.connection.exec_query(pfql.create_main_performance_nodes)
+        add_ecdfcs(db)
+        add_utilizations(db)
+        add_queues(db)
+        add_flows(db)
+    
+    def add_ecdfcs(db):
+        continue
+
+    def add_utilizations(db):
+        continue
+
+    def add_queues(db):
+        continue
+
+    def add_flows(db):
+        continue
+
+    
